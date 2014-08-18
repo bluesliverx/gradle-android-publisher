b763dc49327d1a5cb8f72dbc66e96df0e8c63baf
Brian Saville <bsaville@adaptivecomputing.com>
2014-08-18 09:50:53 -0600

    Add promotion task

--- android-publisher/src/main/groovy/com/savillians/gradle/androidpublisher/AndroidPromoteTask.java
+++ android-publisher/src/main/groovy/com/savillians/gradle/androidpublisher/AndroidPromoteTask.java
@@ -23,7 +23,7 @@ import com.google.api.services.androidpublisher.AndroidPublisher;
 import com.google.api.services.androidpublisher.AndroidPublisher.Edits;
 import com.google.api.services.androidpublisher.AndroidPublisher.Edits.Commit;
 import com.google.api.services.androidpublisher.AndroidPublisher.Edits.Insert;
-import com.google.api.services.androidpublisher.AndroidPublisher.Edits.Get;
+import com.google.api.services.androidpublisher.AndroidPublisher.Edits.Tracks;
 import com.google.api.services.androidpublisher.AndroidPublisher.Edits.Tracks.Update;
 import com.google.api.services.androidpublisher.model.AppEdit;
 import com.google.api.services.androidpublisher.model.Track;
@@ -35,10 +35,12 @@ import org.gradle.api.tasks.TaskAction;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
- * Uploads an apk to the alpha track.
+ * Promotes an APK from one track to another.
  */
 public class AndroidPromoteTask extends DefaultTask {
 	@TaskAction
@@ -73,58 +75,100 @@ public class AndroidPromoteTask extends DefaultTask {
 	}
 
 	private void promoteApk(AndroidPublisherExtension publisherExtension) {
-//		try {
-//			// Create the API service
-//			AndroidPublisher service = AndroidPublisherHelper.init(
-//					publisherExtension.getApplicationName(),
-//					publisherExtension.getServiceAccountEmail(),
-//					publisherExtension.getServiceAccountKeyFile()
-//			);
-//			final Edits getEdits = service.edits();
-//			final Edits promoteEdits = service.edits();
-//
-//			// Create a get edit to get the latest APK version code
-//			Insert getRequest = getEdits.insert(publisherExtension.getPackageName(), null);
-//			AppEdit get = getRequest.execute();
-//			final String getId = get.getId();
-//			getLogger().info(String.format("Created get edit with id: %s", getId));
-//			Get getRequest = getEdits.apklistings().
-//
-//			// Create a new edit to make changes to your listing
-//			Insert editRequest = edits
-//					.insert(publisherExtension.getPackageName(), null /** no content */);
-//			AppEdit edit = editRequest.execute();
-//			final String editId = edit.getId();
-//			getLogger().info(String.format("Created edit with id: %s", editId));
-//
-//			// Assign apk to alpha track.
-//			List<Integer> apkVersionCodes = new ArrayList<Integer>();
-//			apkVersionCodes.add(apk.getVersionCode());
-//			Update updateTrackRequest = edits
-//					.tracks()
-//					.patch()
-//					.update(publisherExtension.getPackageName(),
-//							editId,
-//							publisherExtension.getTrack(),
-//							new Track().setVersionCodes(apkVersionCodes));
-//			Track updatedTrack = updateTrackRequest.execute();
-//			getLogger().info(String.format("Track %s has been updated", updatedTrack.getTrack()));
-//
-//			// Commit changes for edit.
-//			Commit commitRequest = edits.commit(publisherExtension.getPackageName(), editId);
-//			AppEdit appEdit = commitRequest.execute();
-//			getLogger().info(String.format("App edit with id %s has been committed", appEdit.getId()));
-//
-//		} catch (IOException e) {
-//			throw new InvalidUserDataException(
-//					String.format("Exception was thrown while uploading APK to the %s track: %s",
-//							publisherExtension.getTrack(), e.getMessage()),
-//					e);
-//		} catch (GeneralSecurityException e) {
-//			throw new InvalidUserDataException(
-//					String.format("Exception was thrown while uploading APK to the %s track: %s",
-//							publisherExtension.getTrack(), e.getMessage()),
-//					e);
-//		}
+		try {
+			// Create the API service
+			AndroidPublisher service = AndroidPublisherHelper.init(
+					publisherExtension.getApplicationName(),
+					publisherExtension.getServiceAccountEmail(),
+					publisherExtension.getServiceAccountKeyFile()
+			);
+			final Edits edits = service.edits();
+
+			// Create a new edit to make changes to your listing
+			Insert editRequest = edits
+					.insert(publisherExtension.getPackageName(), null /** no content */);
+			AppEdit edit = editRequest.execute();
+			final String editId = edit.getId();
+			getLogger().info(String.format("Created edit with id: %s", editId));
+
+			// List all tracks
+			Tracks.List list = edits.tracks().list(publisherExtension.getPackageName(), editId);
+			List<Track> tracks = list.execute().getTracks();
+
+			// Identify the source and destination tracks
+			Track sourceTrack = null;
+			Track destinationTrack = null;
+			for (Track track : tracks) {
+				if (track.getTrack().equals(publisherExtension.getTrack()))
+					sourceTrack = track;
+				else if (track.getTrack().equals(publisherExtension.getPromotionTrack()))
+					destinationTrack = track;
+			}
+
+			// Error checking
+			if (sourceTrack==null || destinationTrack==null) {
+				throw new InvalidUserDataException(String.format(
+						"Cannot find the %s or %s track on Google Play, invalid track name?",
+						publisherExtension.getTrack(), publisherExtension.getPromotionTrack()
+				));
+			}
+			if (sourceTrack.getVersionCodes().size()==0) {
+				throw new InvalidUserDataException(String.format(
+						"Cannot find a valid APK version code for the %s track, does it have at least one APK already uploaded?",
+						sourceTrack.getVersionCodes()
+				));
+			}
+			getLogger().info("Using source track {} with version codes {}",
+					sourceTrack.getTrack(), sourceTrack.getVersionCodes());
+			getLogger().info("Using destination track {} with version codes {}",
+					destinationTrack.getTrack(), destinationTrack.getVersionCodes());
+
+			// Find version code to promote, remove from source track and add to destination track
+			Integer versionCode = Collections.max(sourceTrack.getVersionCodes());
+			List<Integer> sourceVersionCodes = sourceTrack.getVersionCodes();
+			sourceVersionCodes.remove(versionCode);
+			sourceTrack.setVersionCodes(sourceVersionCodes);
+			List<Integer> destinationVersionCodes = new ArrayList<Integer>();
+			destinationVersionCodes.add(versionCode);
+			destinationTrack.setVersionCodes(destinationVersionCodes);
+			getLogger().info("Promoting version code {}", versionCode);
+
+			Update sourceUpdateRequest = edits
+					.tracks()
+					.update(publisherExtension.getPackageName(),
+							editId,
+							sourceTrack.getTrack(), sourceTrack);
+			sourceUpdateRequest.execute();
+			getLogger().info(String.format("Source track %s has been updated", sourceTrack.getTrack()));
+			Update destinationUpdateRequest = edits
+					.tracks()
+					.update(publisherExtension.getPackageName(),
+							editId,
+							destinationTrack.getTrack(), destinationTrack);
+			getLogger().info(String.format("Destination track %s has been updated",
+					destinationTrack.getTrack()));
+			destinationUpdateRequest.execute();
+
+			// Commit changes for edit.
+			Commit commitRequest = edits.commit(publisherExtension.getPackageName(), editId);
+			AppEdit appEdit = commitRequest.execute();
+			getLogger().info(String.format("App edit with id %s has been committed", appEdit.getId()));
+			getLogger().lifecycle("Version code {} has been promoted from the {} to the {} track",
+					versionCode, sourceTrack.getTrack(), destinationTrack.getTrack());
+		} catch (IOException e) {
+			throw new InvalidUserDataException(
+					String.format("Exception was thrown while promoting APK from the %s track to the %s track: %s",
+							publisherExtension.getTrack(),
+							publisherExtension.getPromotionTrack(),
+							e.getMessage()),
+					e);
+		} catch (GeneralSecurityException e) {
+			throw new InvalidUserDataException(
+					String.format("Exception was thrown while promoting APK from the %s track to the %s track: %s",
+							publisherExtension.getTrack(),
+							publisherExtension.getPromotionTrack(),
+							e.getMessage()),
+					e);
+		}
 	}
 }

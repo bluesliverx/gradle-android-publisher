2a9ffd21034a0743eef3b7953da3697867ddd397
Brian Saville <saville@adobe.com>
2015-03-22 23:34:47 -0600

    Use travis build to add in nexus staging release

---
+++ travis-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+./gradlew clean build uploadArchives
+if [[ $TRAVIS_BRANCH =~ ^[0-9].*$ ]]; then
+	echo "Releasing to maven central"
+	./gradlew nexusStagingRelease
+else
+	echo "Snapshot build"
+fi

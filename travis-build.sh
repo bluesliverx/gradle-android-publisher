7572a25dbd9941f892eff8112db5aac50647dde0
Brian Saville <bksaville@gmail.com>
2015-04-05 08:36:23 -0600

    Revert build back to normal

--- travis-build.sh
+++ travis-build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-./gradlew signArchives
-#./gradlew clean build uploadArchives
-#if [[ $TRAVIS_BRANCH =~ ^[0-9].*$ ]]; then
-#	echo "Releasing to maven central"
-#	./gradlew nexusStagingRelease
-#else
-#	echo "Snapshot build"
-#fi
+./gradlew clean build uploadArchives
+if [[ $TRAVIS_BRANCH =~ ^[0-9].*$ ]]; then
+	echo "Releasing to maven central"
+	./gradlew nexusStagingRelease
+else
+	echo "Snapshot build"
+fi

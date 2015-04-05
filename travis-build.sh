5183dd34414f758f87244aabee3b74378f03ea5a
Brian Saville <bksaville@gmail.com>
2015-04-05 08:11:39 -0600

    Test commit to sign archives correctly

--- travis-build.sh
+++ travis-build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-./gradlew clean build uploadArchives
-if [[ $TRAVIS_BRANCH =~ ^[0-9].*$ ]]; then
-	echo "Releasing to maven central"
-	./gradlew nexusStagingRelease
-else
-	echo "Snapshot build"
-fi
+./gradlew signArchives
+#./gradlew clean build uploadArchives
+#if [[ $TRAVIS_BRANCH =~ ^[0-9].*$ ]]; then
+#	echo "Releasing to maven central"
+#	./gradlew nexusStagingRelease
+#else
+#	echo "Snapshot build"
+#fi
